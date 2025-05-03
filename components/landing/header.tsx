6a86e36eeaf4bf2b60e241318f97cbcc3657bf9b
Tickether <tickether@gmail.com>
2025-05-03 20:57:12 +0000

    feat: basic header

--- components/landing/header.tsx
+++ components/landing/header.tsx
@@ -6,7 +6,41 @@ import { Button } from "../ui/button";
 export function Header() {
     return (
         <div className="flex w-full h-20 justify-between items-center px-4">
-            
+            {/** left */}
+            <div>
+                <Image
+                    src="/icons/512x512.png"
+                    alt=""
+                    width={60}
+                    height={60}
+                />
+            </div>
+
+
+            {/** middle */}
+            <div className="flex gap-14">
+                <div className="flex gap-1 items-center">
+                    <p>Home</p>
+                </div>
+                <div className="flex gap-1 items-center">
+                    <p>Our Services</p>
+                </div>
+                <div className="flex gap-1 items-center">
+                    <p>About Us</p>
+                </div>
+                <div className="flex gap-1 items-center">
+                    <p>FAQs</p>
+                </div>
+                <div></div>
+            </div>
+
+
+            {/** right */}
+            <div className="flex gap-5">
+                <Button className="rounded-full">
+                    <p>Contact us</p>
+                </Button>
+            </div>
         </div>
     );
 }
