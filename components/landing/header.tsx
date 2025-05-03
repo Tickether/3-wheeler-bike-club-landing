1caff21656aaee9037bacc484fa15d57a11f6d15
Tickether <tickether@gmail.com>
2025-05-03 20:40:59 +0000

    feat: strip landing

--- components/landing/header.tsx
+++ components/landing/header.tsx
@@ -1,41 +1,12 @@
 import { BookOpenIcon, Contact, FileQuestion, Home } from "lucide-react";
 import Image from "next/image";
+import { Button } from "../ui/button";
 
 
 export function Header() {
     return (
-        <div className="flex w-full px-40 py-12 justify-between bg-[#191C1F] max-lg:px-8">
-            {/** left */}
-            <div className="">
-                <Image
-                    src="/icons/512x512.png"
-                    alt=""
-                    width={60}
-                    height={60}
-                />
-            </div>
-
-
-            {/** right */}
-            <div className="flex gap-5 text-xl max-5xl:text-lg">
-                <div className="flex gap-1 items-center">
-                    <Home/>
-                    <p className="max-lg:hidden">home</p>
-                </div>
-                <div className="flex gap-1 items-center">
-                    <BookOpenIcon/>
-                    <p className="max-lg:hidden">features</p>
-                </div>
-                <div className="flex gap-1 items-center">
-                    <FileQuestion/>
-                    <p className="max-lg:hidden">about</p>
-                </div>
-                <div className="flex gap-1 items-center">
-                    <Contact/>
-                    <p className="max-lg:hidden">contact us</p>
-                </div>
-                <div></div>
-            </div>
+        <div className="flex w-full h-20 justify-between items-center px-4">
+            
         </div>
     );
 }
--- components/landing/wrapper.tsx
+++ components/landing/wrapper.tsx
@@ -1,21 +1,14 @@
 "use client"
 
-import { About } from "./about";
-import { Contact } from "./contact";
-import { Features } from "./features";
 import { Footer } from "./footer";
 import { Header } from "./header";
-import { Hero } from "./hero";
 
 
 export function Wrapper() {
   return (
     <div className="flex flex-col w-full min-h-screen">
       <Header/>
-      <Hero/>
-      <Features/>
-      <About/>
-      <Contact/>
+
       <Footer/>
     </div>
   );
