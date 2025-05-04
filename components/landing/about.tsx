08837aa86a1a21d3c7a4ffac8936eace85dcfe74
Tickether <tickether@gmail.com>
2025-05-04 19:56:05 +0000

    chore: more mobile opts

--- components/landing/about.tsx
+++ components/landing/about.tsx
@@ -4,13 +4,15 @@ export function About() {
     return (
         <div className="flex max-sm:flex-col w-full mb-36 h-full items-center justify-center gap-20 max-md:gap-10">
                 <div className="flex flex-col gap-4 w-2/5 max-md:w-full max-md:h-2/5 items-center justify-center">
-                    <h1 className="text-4xl max-md:text-xl font-semibold">About Us</h1>
+                    <div className="flex flex-col w-full gap-4">
+                        <h1 className="text-4xl max-md:text-xl font-semibold text-start max-sm:text-center">About Us</h1>
+                    </div>
                     <div className="flex flex-col gap-4">
-                        <p className="text-lg max-md:text-xs max-sm:text-[10px] max-sm:text-center">{"3 Wheeler Bike Club is a Membership Club for 3-Wheeler(TukTuk/Pragia/Keke) Bikers built on the pillars of Ownership, Community & Governance."}</p>
-                        <p className="text-lg max-md:text-xs max-sm:text-[10px] max-sm:text-center">{"A community driven platform for 3-wheelers bikers with membership payment & credit score features, and a P2P finance feature for buying or adding bikes to the platform with hire purchase agreements."}</p>
+                        <p className="text-base max-xl:text-xs max-lg:text-[10px] max-sm:text-center">{"3 Wheeler Bike Club is a Membership Club for 3-Wheeler(TukTuk/Pragia/Keke) Bikers built on the pillars of Ownership, Community & Governance."}</p>
+                        <p className="text-base max-xl:text-xs max-lg:text-[10px] max-sm:text-center">{"A community driven platform for 3-wheelers bikers with membership payment & credit score features, and a P2P finance feature for buying or adding bikes to the platform with hire purchase agreements."}</p>
                     </div>
                 </div>
-                <div className="flex w-2/5 max-md:w-full max-md:h-2/5 items-center justify-center max-sm:w-72 max-sm:h-72">
+                <div className="flex w-2/5 max-md:w-full max-md:h-2/5 items-center justify-center max-lg:w-96 max-lg:h-96 max-sm:w-72 max-sm:h-72">
                     <Image src="/images/kekeAbout.svg" alt="Keke About" width={700} height={700} />
                 </div>
         </div>
--- components/landing/faqs.tsx
+++ components/landing/faqs.tsx
@@ -4,13 +4,13 @@ import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "..
 export function FAQs() {
     return (
         <div className="flex max-sm:flex-col-reverse w-full mb-36 h-full items-center justify-center gap-20">
-            <div className="flex w-2/5 max-md:w-full max-md:h-2/5 items-center justify-center max-sm:w-64 max-sm:h-64">
+            <div className="flex w-2/5 max-md:w-full max-md:h-2/5 items-center justify-center max-lg:w-96 max-lg:h-96 max-sm:w-64 max-sm:h-64">
                 <Image src="/images/kekeFAQs.svg" alt="Keke FAQs" width={700} height={700} />
             </div>
             <div className="flex flex-col gap-6 max-md:gap-2 w-2/5 max-md:w-full max-md:h-2/5 items-center justify-center">
                 <div className="flex flex-col gap-2">
-                    <h1 className="text-3xl max-md:text-xl max-sm:text-base font-semibold max-sm:text-center">Frequently Asked Questions</h1>
-                    <p className="text-sm max-md:text-xs max-sm:text-[10px] max-sm:text-center">Here are some of the most common inquiries we receive to help you make informed decisions with confidence. If you need more details, feel free to reach out!</p>
+                    <h1 className="text-3xl max-xl:text-xl max-lg:text-base font-semibold max-sm:text-center">Frequently Asked Questions</h1>
+                    <p className="text-sm max-xl:text-xs max-lg:text-[10px] max-sm:text-center">Here are some of the most common inquiries we receive to help you make informed decisions with confidence. If you need more details, feel free to reach out!</p>
                 </div>
                 <div className="w-full">
                     <Accordion type="single" collapsible className="flex flex-col w-full gap-1">
